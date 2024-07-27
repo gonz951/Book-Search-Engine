bf7053585fec94418d9a01af6d2bfe325a947ca1
Fel <felgonzalez951@gmail.com>
2024-07-27 15:02:12 -0600

    started resolvers, still need to finish mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,46 @@
 const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
+        me: async (parent, args, context) => {
+            if (context.user) {
+                // ? Subject to change
+                return User.findOne({ _id: context.user.id })
+            }
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            // * This should be fine
+          const user = await User.findOne({email});
+
+          if (!user) {
+            throw AuthenticationError;
+          }
+
+          const correctPw = await user.isCorrectPassword(password);
+          if (!correctPw) {
+            throw AuthenticationError;
+          }
+
+          const token = signToken(user);
+
+          return { token, user };
+        },
+        // This needs the username, email, and password
+        addUser: async (parent, { username, email, password }) {
+            // ! NEEDS TO BE FINISHED
+        },
+        // This has the bookInput for it
+        saveBook: async (parent) {
+            // ! NEEDS TO BE FINISHED
+        },
+        // this only needs bookId
+        removeBook: async (parent, {bookId}) {
+            // ! NEEDS TO BE FINISHED
+        }
     }
 }
+
+module.exports = resolvers;
