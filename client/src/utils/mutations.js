111f6d9a2ae750968dbda42fda7f530cabf273d9
Fel <felgonzalez951@gmail.com>
2024-07-27 15:13:29 -0600

    started mutations file

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    // add mutations later
+`;
+
+export const ADD_USER = gql`
+    // add mutations later
+`;
+
+export const SAVE_BOOK = gql`
+    // add mutations later
+`;
+
+export const REMOVE_BOOK = gql`
+    // add mutations later
+`;
