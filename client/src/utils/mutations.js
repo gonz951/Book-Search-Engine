61631745ba7ffa460a90e586c6c27144bb7c5869
Fel <felgonzalez951@gmail.com>
2024-07-29 16:28:46 -0600

    changed mutation method for LOGIN_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,20 +3,23 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
-            _id
-            email
-            password
+            token
+            user {
+                _id
+                username
+            }
         }
     }
 `;
 
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password){
-            _id 
-            username
-            email
-            password
+        addUser(username: $username, email: $email, password: $password) {
+            token 
+            user {
+                _id
+                username
+            }
         }
     }
 `;
