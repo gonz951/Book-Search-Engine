5c675cc73caa5b825885115740544c89571c7203
Fel <felgonzalez951@gmail.com>
2024-07-31 18:51:06 -0600

    added saveBook method

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,34 +14,37 @@ const resolvers = {
     Mutation: {
         login: async (parent, { email, password }) => {
             // * This should be fine
-          const user = await User.findOne({ email });
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw AuthenticationError;
+            }
 
-          if (!user) {
-            throw AuthenticationError;
-          }
-
-          const correctPw = await user.isCorrectPassword(password);
-          if (!correctPw) {
-            throw AuthenticationError;
-          }
-
-          const token = signToken(user);
-
-          return { token, user };
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+            const token = signToken(user);
+            return { token, user };
         },
-        // This needs the username, email, and password
+        // * This should be fine
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
-        // // This has the bookInput for it
-        // saveBook: async (parent) {
-        //     // ! NEEDS TO BE FINISHED
-        // },
-        // // this only needs bookId
-        // removeBook: async (parent, {bookId}) {
-        //     // ! NEEDS TO BE FINISHED
+        // This has the bookInput for it
+        saveBook: async (parent, args, context) => {
+            const book = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: args.input } },
+                { new: true }
+            );
+
+            return book;
+        },
+        // this only needs bookId
+        // removeBook: async (parent, {bookId}) => {
+            
         // }
     }
 }
